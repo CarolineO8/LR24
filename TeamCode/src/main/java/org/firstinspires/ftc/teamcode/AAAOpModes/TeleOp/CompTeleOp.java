6317565385a0709dc934769865d6817e35e37f2b
OliverB11 <92954010+OliverB11@users.noreply.github.com>
2024-01-09 18:36:28 -0500

    started coding for lupine stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAOpModes/TeleOp/CompTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAOpModes/TeleOp/CompTeleOp.java
@@ -18,15 +18,25 @@ public class CompTeleOp extends BaseOpMode {
     public void externalInit() {
         //On init
         fl = BaseOpMode.hardware.get(DcMotor.class,"fl");
-        fl = BaseOpMode.hardware.get(DcMotor.class,"fr");
-        fl = BaseOpMode.hardware.get(DcMotor.class,"br");
-        fl = BaseOpMode.hardware.get(DcMotor.class,"bl");
+        fr = BaseOpMode.hardware.get(DcMotor.class,"fr");
+        br = BaseOpMode.hardware.get(DcMotor.class,"br");
+        bl = BaseOpMode.hardware.get(DcMotor.class,"bl");
+
 
     }
 
     @Override
     public void externalLoop() {
-
         //After start
+        double drive = driver1.leftStick.Y();
+        double strafe = driver1.leftStick.X();
+        double turn = driver1.rightStick.X();
+        double speed = 0.5;
+
+        fl.setPower(-(drive - strafe + turn) * speed);
+        fr.setPower((drive + strafe - turn) * speed);
+        bl.setPower(-(drive + strafe + turn) * speed);
+        br.setPower((drive - strafe - turn) * speed);
     }
+
 }
