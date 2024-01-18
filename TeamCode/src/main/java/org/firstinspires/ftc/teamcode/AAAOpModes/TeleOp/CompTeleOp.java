eec234b044d391b179747247e39c2dbdfcadacfb
OliverB11 <92954010+OliverB11@users.noreply.github.com>
2024-01-18 16:03:16 -0500

    added gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAOpModes/TeleOp/CompTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAOpModes/TeleOp/CompTeleOp.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.AAAOpModes.TeleOp;
 
+import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.AAAOpModes.BaseOpMode;
+import org.firstinspires.ftc.teamcode.zLibraries.Utilities.HardwareDevices.Gyro;
 
 //@Disabled
 @TeleOp(name="AAA COMPETITION TELEOP", group="Iterative Opmode")
@@ -13,6 +15,7 @@ public class CompTeleOp extends BaseOpMode {
     DcMotor fr;
     DcMotor bl;
     DcMotor br;
+    Gyro gyro;
 
     @Override
     public void externalInit() {
@@ -21,6 +24,7 @@ public class CompTeleOp extends BaseOpMode {
         fr = BaseOpMode.hardware.get(DcMotor.class,"fr");
         br = BaseOpMode.hardware.get(DcMotor.class,"br");
         bl = BaseOpMode.hardware.get(DcMotor.class,"bl");
+        gyro = new Gyro("imu");
 
 
     }
@@ -32,6 +36,13 @@ public class CompTeleOp extends BaseOpMode {
         double strafe = driver1.leftStick.X();
         double turn = driver1.rightStick.X();
         double speed = 0.5;
+
+        Vector2d driveVector = new Vector2d(strafe, drive);
+        Vector2d rotatedVector = driveVector.rotateBy(Math.toDegrees(270-gyro.getHeading()));
+
+
+        drive = rotatedVector.getY();
+        strafe = -rotatedVector.getX();
 
         fl.setPower(-(drive - strafe + turn) * speed);
         fr.setPower((drive + strafe - turn) * speed);
